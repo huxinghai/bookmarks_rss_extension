42e67804b4c4129253ed35c91d357c4fa539821f
kaka <huxinghai1988@gmail.com>
2016-09-04 21:42:54 +0800

    using websocket online notify

--- background.js
+++ background.js
@@ -1,123 +1,134 @@
-console.log("load bookmarks rss background");
+(function() {
+  console.log("load bookmarks rss background");
+  var api_domain = "http://localhost:3000",
+    currentUser = null;
 
-var api_domain = "http://localhost:3000",
-  currentUser = null;
+  var remoteRequest = function(url, params, method, callback){
+    var m = "GET";
 
-var remoteRequest = function(url, params, method, callback){
-  var m = "GET";
+    if(~["GET", "POST", "DELETE", "PUT"].indexOf(params)){
+      m = params
+      params = {}
+    }
 
-  if(~["GET", "POST", "DELETE", "PUT"].indexOf(params)){
-    m = params
-    params = {}
+    if(typeof method == "function")
+      callback = method
+    else if(method)
+      m = method
+
+    var options = JSON.stringify(params);
+    var xhr = new XMLHttpRequest();
+    xhr.open(m, url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    // if(currentUser) xhr.setRequestHeader("X-CSRF-Token", currentUser.authenticity_token)
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        var resp = JSON.parse((xhr.responseText == "" ? "{}" : xhr.responseText));
+        callback && callback(resp);
+      }
+    }
+    xhr.send(options);
   }
 
-  if(typeof method == "function")
-    callback = method
-  else if(method)
-    m = method
+  var fetchApiRequest = function(){
+    var data = Array.prototype.slice.call(arguments)
+    data[0] = api_domain + data[0]
+    remoteRequest.apply(this, data)
+  }
 
-  var options = JSON.stringify(params);
-  var xhr = new XMLHttpRequest();
-  xhr.open(m, url, true);
-  xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
-  // if(currentUser) xhr.setRequestHeader("X-CSRF-Token", currentUser.authenticity_token)
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      var resp = JSON.parse((xhr.responseText == "" ? "{}" : xhr.responseText));
-      callback && callback(resp);
+  var signUp = function(userInfo, callback){
+    userInfo.provision_id = userInfo.id
+    fetchApiRequest("/api/v1/users", {
+      user: userInfo
+    }, "POST", function(res){
+      console.log("sign up", res)
+      currentUser = res 
+      callback && callback(currentUser)
+    }) 
+  }
+
+  var remoteCreateBookmark = function(data, callback){
+    fetchApiRequest("/api/v1/bookmarks", {bookmarks: data}, "POST", function(data){
+      callback && callback(data)
+    })
+  }
+
+  var get_bookmarks = function(bookmark, items, date_added_limit){
+    if(!items) items = []
+    if(!date_added_limit) date_added_limit = 0
+    if(bookmark.url && bookmark.dateAdded > date_added_limit){
+      items.push({
+        provision_id: bookmark.id,
+        parent_id: bookmark.parentId,
+        index: bookmark.index,
+        title: bookmark.title,
+        url: bookmark.url,
+        date_added: bookmark.dateAdded,
+        date_group_modified: bookmark.dateGroupModified
+      })
+    }
+
+    if(bookmark.children && bookmark.children.length > 0){
+      bookmark.children.forEach(function(bm){
+        get_bookmarks(bm, items, date_added_limit)
+      })
     }
   }
-  xhr.send(options);
-}
 
-var fetchApiRequest = function(){
-  var data = Array.prototype.slice.call(arguments)
-  data[0] = api_domain + data[0]
-  remoteRequest.apply(this, data)
-}
+  chrome.identity.getProfileUserInfo(function(userInfo) {
 
-var signUp = function(userInfo, callback){
-  userInfo.provision_id = userInfo.id
-  fetchApiRequest("/api/v1/users", {
-    user: userInfo
-  }, "POST", function(res){
-    console.log("sign up", res)
-    currentUser = res 
-    callback && callback(currentUser)
-  }) 
-}
+    signUp(userInfo, function(res){
 
-var remoteCreateBookmark = function(data, callback){
-  fetchApiRequest("/api/v1/bookmarks", {bookmarks: data}, "POST", function(data){
-    callback && callback(data)
-  })
-}
+      chrome.bookmarks.getTree(function(bookmarks){
+        var res_bookmarks = []
+        bookmarks.forEach(function(bookmark){
+          get_bookmarks(bookmark, res_bookmarks, currentUser.last_date_added)
+        })
+        if(res_bookmarks.length > 0) remoteCreateBookmark(res_bookmarks)
+      });
 
-var get_bookmarks = function(bookmark, items, date_added_limit){
-  if(!items) items = []
-  if(!date_added_limit) date_added_limit = 0
-  if(bookmark.url && bookmark.dateAdded > date_added_limit){
-    items.push({
-      provision_id: bookmark.id,
-      parent_id: bookmark.parentId,
-      index: bookmark.index,
-      title: bookmark.title,
-      url: bookmark.url,
-      date_added: bookmark.dateAdded,
-      date_group_modified: bookmark.dateGroupModified
+      chrome.bookmarks.onCreated.addListener(function(id, bookmark){
+        var data = []
+        get_bookmarks(bookmark, data)
+        if(data.length > 0) remoteCreateBookmark(data)
+      });
+
+      chrome.bookmarks.onRemoved.addListener(function(id, bookmark){
+        fetchApiRequest("/api/v1/bookmarks/"+ id, "DELETE")
+      });
+
+      setDisplayBadge(currentUser.badge_number)
+      loadCable(currentUser);
     })
-  }
 
-  if(bookmark.children && bookmark.children.length > 0){
-    bookmark.children.forEach(function(bm){
-      get_bookmarks(bm, items, date_added_limit)
-    })
-  }
-}
-
-chrome.identity.getProfileUserInfo(function(userInfo) {
-
-  signUp(userInfo, function(res){
-
-    chrome.bookmarks.getTree(function(bookmarks){
-      var res_bookmarks = []
-      bookmarks.forEach(function(bookmark){
-        get_bookmarks(bookmark, res_bookmarks, currentUser.last_date_added)
-      })
-      if(res_bookmarks.length > 0) remoteCreateBookmark(res_bookmarks)
+    chrome.extension.onMessage.addListener(function() {
+      console.log("simple request onMessage")
+      console.log(arguments)
     });
 
-    chrome.bookmarks.onCreated.addListener(function(id, bookmark){
-      var data = []
-      get_bookmarks(bookmark, data)
-      if(data.length > 0) remoteCreateBookmark(data)
-    });
-
-    chrome.bookmarks.onRemoved.addListener(function(id, bookmark){
-      fetchApiRequest("/api/v1/bookmarks/"+ id, "DELETE")
-    });
-
-  })
-
-  chrome.extension.onMessage.addListener(function() {
-    console.log("simple request onMessage")
-    console.log(arguments)
   });
 
-});
 
-var setDisplayBadge = function(number){
-  if(number && number > 0){
-    chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]})
+  var loadCable = function(user){
+    var cable = ActionCable.createConsumer();
+
+    cable.subscriptions.create({channel: 'ArticlesChannel', room: user.room_name}, {
+      received: function(data){
+        setDisplayBadge(data.number)
+      }
+    })
   }
-  chrome.browserAction.setBadgeText({text: (number || "").toString()})
-}
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({ url: api_domain + "/articles" });
-  setDisplayBadge()
-});
+  var setDisplayBadge = function(number){
+    if(number && number > 0){
+      chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]})
+    }
+    chrome.browserAction.setBadgeText({text: (number || "").toString()})
+  }
 
-setDisplayBadge(102)
+  chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({ url: api_domain + "/articles" });
+    setDisplayBadge()
+  });
 
-
+}).call(this);
